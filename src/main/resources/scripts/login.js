8b716c70f7b03ca6ce31dd805fd5bf99acc686c6
Eddie <eduard-kagrimanyan@hotmail.com>
2016-11-02 18:38:32 -0700

    Implemented register

--- src/main/resources/scripts/login.js
+++ src/main/resources/scripts/login.js
@@ -1,10 +1,28 @@
 function registerUser() {
-
-
-
+	var username = $('.login-register-form').find('#username').val();
+	var password = $('.login-register-form').find('#password').val();
+	if (username === null || username === '') {
+		alert('Please enter a username.');
+	} else if (password === null || password === '') {
+		alert('Please enter a password.');
+	} else {
+		$.ajax({
+		   url: '/user/',
+		   type: 'POST',
+		   data: {
+		      username: username,
+		      password: password
+		   },
+		   success: function(data) {
+		     
+		   },
+		   error: function() {
+		      
+		   },
+		});
+	}
 }
 
 function loginUser(){
-
-	alert();
+	
 }
